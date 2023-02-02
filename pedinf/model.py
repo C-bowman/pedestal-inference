25887adb965614d00592423bcd489d9a5f1dd318
C-bowman <chris.bowman.physics@gmail.com>
2023-02-02 18:16:32 +0000

    Added prototype forward model for spectrometer data

--- pedinf/model.py
+++ pedinf/model.py
@@ -1,4 +1,5 @@
-from numpy import exp, log, zeros
+from numpy import exp, log, ndarray, zeros
+from scipy.interpolate import RectBivariateSpline
 
 
 def mtanh(R, theta):
@@ -33,7 +34,7 @@ def mtanh(R, theta):
     """
     R0, h, w, a, b, ln_k = theta
     z = (R - R0) / w
-    G = 1 - (a*w) * z
+    G = 1 - (a * w) * z
     L = (1 + exp(4 * z)) ** -exp(ln_k)
     return (h * (1 - b)) * (G * L) + h * b
 
@@ -60,12 +61,12 @@ def mtanh_gradient(R, theta):
     G = 1 - (a * w) * z
     exp_4z = exp(4 * z)
     L0 = 1 + exp_4z
-    L = L0 ** -k
+    L = L0**-k
 
     return -(h * (1 - b)) * (G * ((4 * k / w) * exp_4z / L0) + a) * L
 
 
-class PedestalModel(object):
+class PedestalModel:
     def __init__(self, R):
         self.R = R
 
@@ -81,16 +82,72 @@ class PedestalModel(object):
         G = 1 - (a * w) * z
         exp_4z = exp(4 * z)
         L0 = 1 + exp_4z
-        L = L0 ** -k
+        L = L0**-k
         GL = G * L
-        Q = (exp_4z/L0)*GL
+        Q = (exp_4z / L0) * GL
 
         # fill the jacobian with derivatives of the prediction w.r.t. each parameter
         jac = zeros([self.R.size, 6])
-        jac[:, 0] = (h*(1 - b))*(a*L + (4*k/w)*Q)
-        jac[:, 1] = (1 - b)*GL + b
-        jac[:, 2] = (4*k*h*(1 - b)/w) * Q * z
-        jac[:, 3] = -z * (w*h*(1 - b)) * L
-        jac[:, 4] = h*(1 - GL)
-        jac[:, 5] = -(h*(1 - b)) * GL * log(L0) * k
+        jac[:, 0] = (h * (1 - b)) * (a * L + (4 * k / w) * Q)
+        jac[:, 1] = (1 - b) * GL + b
+        jac[:, 2] = (4 * k * h * (1 - b) / w) * Q * z
+        jac[:, 3] = -z * (w * h * (1 - b)) * L
+        jac[:, 4] = h * (1 - GL)
+        jac[:, 5] = -(h * (1 - b)) * GL * log(L0) * k
         return jac
+
+
+class SpectrometerModel:
+    def __init__(
+        self,
+        response_spline_intensity: ndarray,
+        response_spline_ln_te: ndarray,
+        response_spline_theta: ndarray,
+        inst_func_weights: ndarray,
+        inst_func_major_radii: ndarray,
+        inst_func_theta: ndarray,
+    ):
+        self.spline_intensity = response_spline_intensity
+        self.spline_ln_te = response_spline_ln_te
+        self.spline_theta = response_spline_theta
+        self.IF_weights = inst_func_weights
+        self.IF_radius = inst_func_major_radii
+        self.IF_theta = inst_func_theta
+
+        # make sure the instrument function weights are normalised
+        self.IF_weights /= self.IF_weights.sum(axis=1)[:, None]
+
+        self.n_positions = self.spline_intensity.shape[0]
+        self.n_spectra = self.spline_intensity.shape[1]
+
+        self.te_slc = slice(0, 6)
+        self.ne_slc = slice(6, 12)
+
+        # build the splines for all spatial / spectral channels
+        self.splines = []
+        for i in range(self.n_positions):
+            self.splines.append(
+                [
+                    RectBivariateSpline(
+                        x=self.spline_ln_te[i, :],
+                        y=self.spline_theta[i, :],
+                        z=self.spline_intensity[i, j, :, :],
+                    )
+                    for j in range(self.n_spectra)
+                ]
+            )
+
+    def spectrum(self, Te: ndarray, ne: ndarray) -> ndarray:
+        ln_te = log(Te)
+        y = zeros([self.n_positions, self.n_spectra])
+        coeffs = ne * self.IF_weights
+        for i in range(self.n_positions):
+            for j in range(self.n_spectra):
+                response = self.splines[i][j].ev(ln_te[i, :], self.IF_theta[i, :])
+                y[i, j] = (response * coeffs[i, :]).sum()
+        return y
+
+    def predictions(self, theta: ndarray) -> ndarray:
+        Te = mtanh(self.IF_radius, theta[self.te_slc])
+        ne = mtanh(self.IF_radius, theta[self.ne_slc])
+        return self.spectrum(Te, ne).flatten()
