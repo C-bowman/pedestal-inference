0921a8b731cf8e059001c8f97d39082518fda037
C-bowman <chris.bowman.physics@gmail.com>
2023-02-14 10:19:50 +0000

    Further work on model jacobians / gradients

--- pedinf/model.py
+++ pedinf/model.py
@@ -1,5 +1,6 @@
 from numpy import exp, log, ndarray, zeros
 from scipy.interpolate import RectBivariateSpline
+from itertools import product
 
 
 def mtanh(R, theta):
@@ -76,24 +77,53 @@ def lpm(R, theta):
     return (G + L) + b
 
 
+def lpm_gradient(R, theta):
+    R0, h, w, a, b, ln_k = theta
+    k = exp(ln_k)
+    z = 4 * (R - R0) / w
+    L = 1 / (1 + exp(z))
+    return -(4 * (h - b) * k / w) * (1 - L) * L**k - a*L
+
+
 def lpm_jacobian(R, theta):
     R0, h, w, a, b, ln_k = theta
     k = exp(ln_k)
     z = 4 * (R - R0) / w
     L = 1 / (1 + exp(z))
-    S = log(1 + exp(-z))  # think this can be written in terms of L and z
+    ln_L = log(L)
+    S = -ln_L - z
     Lk = L**k
 
     jac = zeros([R.size, 6])
-
-    df_dz_w = (k * (h - b) / w) * Lk * (1 - L) + (0.25 * a) * L
-    jac[:, 0] = -4 * df_dz_w
+    df_dz = (k * (h - b)) * Lk * (1 - L) + (0.25 * a * w) * L
+    jac[:, 0] = (4 / w) * df_dz
     jac[:, 1] = Lk
-    jac[:, 2] = z * df_dz_w
+    jac[:, 2] = (z / w) * df_dz + (0.25 * a) * S
     jac[:, 3] = (0.25 * w) * S
     jac[:, 4] = 1 - Lk
-    jac[:, 5] = (k * (h - b)) * Lk * log(L)
+    jac[:, 5] = (k * (h - b)) * Lk * ln_L
     return jac
+
+
+def lpm_value_and_jacobian(R, theta):
+    R0, h, w, a, b, ln_k = theta
+    k = exp(ln_k)
+    z = 4 * (R - R0) / w
+    L = 1 / (1 + exp(z))
+    ln_L = log(L)
+    S = -ln_L - z
+    Lk = L**k
+
+    prediction = Lk * (h - b) + (0.25 * a * w) * S + b
+    jac = zeros([R.size, 6])
+    df_dz = (k * (h - b)) * Lk * (1 - L) + (0.25 * a * w) * L
+    jac[:, 0] = (4 / w) * df_dz
+    jac[:, 1] = Lk
+    jac[:, 2] = (z / w) * df_dz + (0.25 * a) * S
+    jac[:, 3] = (0.25 * w) * S
+    jac[:, 4] = 1 - Lk
+    jac[:, 5] = (k * (h - b)) * Lk * ln_L
+    return prediction, jac
 
 
 class PedestalModel:
@@ -149,6 +179,7 @@ class SpectrometerModel:
 
         self.n_positions = self.spline_intensity.shape[0]
         self.n_spectra = self.spline_intensity.shape[1]
+        self.n_weights = self.IF_weights.shape[1]
 
         self.te_slc = slice(0, 6)
         self.ne_slc = slice(6, 12)
@@ -177,7 +208,52 @@ class SpectrometerModel:
                 y[i, j] = (response * coeffs[i, :]).sum()
         return y
 
+    def spectrum_jacobian(self, Te: ndarray, ne: ndarray):
+        ln_te = log(Te)
+        te_jac = zeros([self.n_positions, self.n_spectra, self.n_positions, self.n_weights])
+        ne_jac = zeros([self.n_positions, self.n_spectra, self.n_positions, self.n_weights])
+        coeffs = ne * Te * self.IF_weights
+        for i in range(self.n_positions):
+            for j in range(self.n_spectra):
+                ne_jac[i, j, i, :] = self.IF_weights[i, :] * self.splines[i][j].ev(ln_te[i, :], self.IF_theta[i, :])
+                te_jac[i, j, i, :] = coeffs[i, :] * self.splines[i][j].ev(ln_te[i, :], self.IF_theta[i, :], dx=1)
+        te_jac.resize([self.n_positions*self.n_spectra, self.n_positions*self.n_weights])
+        ne_jac.resize([self.n_positions*self.n_spectra, self.n_positions*self.n_weights])
+        return te_jac, ne_jac
+
+    def spectrum_jacobian_alt(self, Te: ndarray, ne: ndarray):
+        ln_te = log(Te)
+        te_jac = zeros([self.n_positions*self.n_spectra, self.n_positions*self.n_weights])
+        ne_jac = zeros([self.n_positions*self.n_spectra, self.n_positions*self.n_weights])
+        coeffs = ne * Te * self.IF_weights
+        itr1 = enumerate(product(range(self.n_positions, self.n_spectra)))
+        itr2 = enumerate(product(range(self.n_positions, self.n_weights)))
+        for a, (i, j) in itr1:
+            for b, (k, l) in itr2:
+                if i == k:
+                    ne_jac[a, b] = self.IF_weights[i, l] * self.splines[i][j].ev(ln_te[i, l], self.IF_theta[i, l])
+                    te_jac[a, b] = coeffs[i, l] * self.splines[i][j].ev(ln_te[i, l], self.IF_theta[i, l], dx=1)
+        return te_jac, ne_jac
+
     def predictions(self, theta: ndarray) -> ndarray:
         Te = lpm(self.IF_radius, theta[self.te_slc])
         ne = lpm(self.IF_radius, theta[self.ne_slc])
         return self.spectrum(Te, ne).flatten()
+
+    def predictions_jacobian(self, theta: ndarray) -> ndarray:
+        Te, lpm_te_jac = lpm_value_and_jacobian(self.IF_radius.flatten(), theta[self.te_slc])
+        ne, lpm_ne_jac = lpm_value_and_jacobian(self.IF_radius.flatten(), theta[self.ne_slc])
+        spec_te_jac, spec_ne_jac = self.spectrum_jacobian(
+            Te.reshape([self.n_positions, self.n_weights]),
+            ne.reshape([self.n_positions, self.n_weights])
+        )
+
+        spec_te_jac_alt, spec_ne_jac_alt = self.spectrum_jacobian_alt(
+            Te.reshape([self.n_positions, self.n_weights]),
+            ne.reshape([self.n_positions, self.n_weights])
+        )
+
+        jac = zeros([self.n_positions * self.n_spectra, 12])
+        jac[:, :6] = spec_te_jac_alt @ lpm_te_jac
+        jac[:, 6:] = spec_ne_jac_alt @ lpm_ne_jac
+        return jac
