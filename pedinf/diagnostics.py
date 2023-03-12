1428277e0ebf569b4093ed7684444bbcbf7ba8a4
C-bowman <chris.bowman.physics@gmail.com>
2023-03-12 11:32:27 +0000

    removed jacobian code from spectrometer model to be developed on a dedicated branch

--- pedinf/diagnostics.py
+++ pedinf/diagnostics.py
@@ -1,6 +1,5 @@
 from numpy import log, ndarray, zeros
 from scipy.interpolate import RectBivariateSpline
-from itertools import product
 from pedinf.models import ProfileModel
 
 
@@ -57,52 +56,7 @@ class SpectrometerModel:
                 y[i, j] = (response * coeffs[i, :]).sum()
         return y
 
-    def spectrum_jacobian(self, Te: ndarray, ne: ndarray):
-        ln_te = log(Te)
-        te_jac = zeros([self.n_positions, self.n_spectra, self.n_positions, self.n_weights])
-        ne_jac = zeros([self.n_positions, self.n_spectra, self.n_positions, self.n_weights])
-        coeffs = ne * Te * self.IF_weights
-        for i in range(self.n_positions):
-            for j in range(self.n_spectra):
-                ne_jac[i, j, i, :] = self.IF_weights[i, :] * self.splines[i][j].ev(ln_te[i, :], self.IF_theta[i, :])
-                te_jac[i, j, i, :] = coeffs[i, :] * self.splines[i][j].ev(ln_te[i, :], self.IF_theta[i, :], dx=1)
-        te_jac.resize([self.n_positions*self.n_spectra, self.n_positions*self.n_weights])
-        ne_jac.resize([self.n_positions*self.n_spectra, self.n_positions*self.n_weights])
-        return te_jac, ne_jac
-
-    def spectrum_jacobian_alt(self, Te: ndarray, ne: ndarray):
-        ln_te = log(Te)
-        te_jac = zeros([self.n_positions*self.n_spectra, self.n_positions*self.n_weights])
-        ne_jac = zeros([self.n_positions*self.n_spectra, self.n_positions*self.n_weights])
-        coeffs = ne * Te * self.IF_weights
-        itr1 = enumerate(product(range(self.n_positions, self.n_spectra)))
-        itr2 = enumerate(product(range(self.n_positions, self.n_weights)))
-        for a, (i, j) in itr1:
-            for b, (k, l) in itr2:
-                if i == k:
-                    ne_jac[a, b] = self.IF_weights[i, l] * self.splines[i][j].ev(ln_te[i, l], self.IF_theta[i, l])
-                    te_jac[a, b] = coeffs[i, l] * self.splines[i][j].ev(ln_te[i, l], self.IF_theta[i, l], dx=1)
-        return te_jac, ne_jac
-
     def predictions(self, theta: ndarray) -> ndarray:
         Te = self.model.prediction(self.IF_radius, theta[self.te_slc])
         ne = self.model.prediction(self.IF_radius, theta[self.ne_slc])
         return self.spectrum(Te, ne).flatten()
-
-    def predictions_jacobian(self, theta: ndarray) -> ndarray:
-        Te, lpm_te_jac = self.model.prediction_and_jacobian(self.IF_radius.flatten(), theta[self.te_slc])
-        ne, lpm_ne_jac = self.model.prediction_and_jacobian(self.IF_radius.flatten(), theta[self.ne_slc])
-        spec_te_jac, spec_ne_jac = self.spectrum_jacobian(
-            Te.reshape([self.n_positions, self.n_weights]),
-            ne.reshape([self.n_positions, self.n_weights])
-        )
-
-        spec_te_jac_alt, spec_ne_jac_alt = self.spectrum_jacobian_alt(
-            Te.reshape([self.n_positions, self.n_weights]),
-            ne.reshape([self.n_positions, self.n_weights])
-        )
-
-        jac = zeros([self.n_positions * self.n_spectra, 12])
-        jac[:, :6] = spec_te_jac_alt @ lpm_te_jac
-        jac[:, 6:] = spec_ne_jac_alt @ lpm_ne_jac
-        return jac
