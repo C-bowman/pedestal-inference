581b8a7dcfb3a1a26f9797bb684aa971a295d034
C-bowman <chris.bowman.physics@gmail.com>
2023-10-05 18:41:44 +0100

    implemented forward model methods using partials

--- pedinf/models/logispline.py
+++ pedinf/models/logispline.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+from functools import partial
 from numpy import arange, concatenate, exp, ndarray, zeros, diff
 from pedinf.models import ProfileModel
 
@@ -7,9 +9,6 @@ class logispline(ProfileModel):
 
     def __init__(self, knots: ndarray, radius=None, low_field_side=True):
         self.knots = knots
-        if radius is not None:
-            self.radius = radius
-            self.basis = b_spline_basis(self.radius, self.knots)
         self.drn = -1 if low_field_side else 1
 
         self.n_parameters = 3 + self.knots.size
@@ -20,53 +19,67 @@ class logispline(ProfileModel):
             "basis_weights": slice(3, self.n_parameters)
         }
 
-    def prediction(self, R: ndarray, theta: ndarray) -> ndarray:
-        basis = b_spline_basis(R, self.knots)
-        z = (4 * self.drn) * (R - theta[0]) / theta[2]
+        if radius is not None:
+            self.radius = radius
+            self.basis, self.derivs = b_spline_basis(self.radius, self.knots, derivatives=True)
+            self.forward_prediction = partial(self._prediction, self.radius, self.basis)
+            self.forward_gradient = partial(self._gradient, self.radius, self.derivs)
+            self.forward_jacobian = partial(self._jacobian, self.radius, self.basis)
+            self.forward_prediction_and_jacobian = partial(self._prediction_and_jacobian, self.radius, self.basis)
+
+    def prediction(self, radius: ndarray, theta: ndarray) -> ndarray:
+        basis = b_spline_basis(radius, self.knots)
+        return self._prediction(radius, basis, theta)
+
+    def gradient(self, radius: ndarray, theta: ndarray) -> ndarray:
+        _, derivs = b_spline_basis(radius, self.knots, derivatives=True)
+        return self._gradient(radius, derivs, theta)
+
+    def jacobian(self, radius: ndarray, theta: ndarray) -> ndarray:
+        basis = b_spline_basis(radius, self.knots)
+        return self._jacobian(radius, basis, theta)
+
+    def prediction_and_jacobian(self, radius: ndarray, theta: ndarray) -> Tuple[ndarray, ndarray]:
+        basis = b_spline_basis(radius, self.knots)
+        return self._prediction_and_jacobian(radius, basis, theta)
+
+    def _prediction(self, radius: ndarray, basis: ndarray, theta: ndarray) -> ndarray:
+        z = (4 * self.drn) * (radius - theta[0]) / theta[2]
         logistic = theta[1] / (1 + exp(-z))
         background = basis @ theta[3:]
         return logistic + background
 
-    def forward_prediction(self, theta: ndarray) -> ndarray:
-        z = (4 * self.drn) * (self.radius - theta[0]) / theta[2]
-        logistic = theta[1] / (1 + exp(-z))
-        background = self.basis @ theta[3:]
-        return logistic + background
-
-    def jacobian(self, R: ndarray, theta: ndarray):
-        basis = b_spline_basis(R, self.knots)
-        z = (4 * self.drn) * (R - theta[0]) / theta[2]
+    def _jacobian(self, radius: ndarray, basis: ndarray, theta: ndarray):
+        z = (4 * self.drn) * (radius - theta[0]) / theta[2]
         L = 1 / (1 + exp(-z))
         q = L * (1 - L)
 
-        jac = zeros([R.size, self.n_parameters])
+        jac = zeros([radius.size, self.n_parameters])
         jac[:, 0] = -q * ((4 * self.drn * theta[1]) / theta[2])
         jac[:, 1] = L
         jac[:, 2] = -q * z * (theta[1] / theta[2])
         jac[:, self.parameters["basis_weights"]] = basis
         return jac
 
-    def prediction_and_jacobian(self, R: ndarray, theta: ndarray):
-        basis = b_spline_basis(R, self.knots)
-        z = (4 * self.drn) * (R - theta[0]) / theta[2]
+    def _prediction_and_jacobian(self, radius: ndarray, basis: ndarray, theta: ndarray):
+        z = (4 * self.drn) * (radius - theta[0]) / theta[2]
         L = 1 / (1 + exp(-z))
         q = L * (1 - L)
 
         prediction = theta[1] * L + basis @ theta[3:]
 
-        jac = zeros([R.size, self.n_parameters])
+        jac = zeros([radius.size, self.n_parameters])
         jac[:, 0] = -q * ((4 * self.drn * theta[1]) / theta[2])
         jac[:, 1] = L
         jac[:, 2] = -q * z * (theta[1] / theta[2])
         jac[:, self.parameters["basis_weights"]] = basis
         return prediction, jac
 
-    def gradient(self, R: ndarray, theta: ndarray):
-        _, derivs = b_spline_basis(R, self.knots, derivatives=True)
-        z = (4 * self.drn) * (R - theta[0]) / theta[2]
+    def _gradient(self, radius: ndarray, basis_derivs: ndarray, theta: ndarray):
+        z = (4 * self.drn) * (radius - theta[0]) / theta[2]
         L = 1 / (1 + exp(-z))
         q = L * (1 - L)
-        return q * (4 * self.drn * theta[1] / theta[2]) + derivs @ theta[3:]
+        return q * (4 * self.drn * theta[1] / theta[2]) + basis_derivs @ theta[3:]
 
 
 def b_spline_basis(x: ndarray, knots: ndarray, order=3, derivatives=False) -> ndarray:
--- pedinf/models/mtanh.py
+++ pedinf/models/mtanh.py
@@ -1,4 +1,5 @@
 from typing import Tuple
+from functools import partial
 from numpy import ndarray, exp, zeros
 from pedinf.models.base import ProfileModel
 
@@ -45,6 +46,12 @@ class mtanh(ProfileModel):
 
     def __init__(self, radius=None, low_field_side=True):
         self.drn = -1 if low_field_side else 1
+        if radius is not None:
+            self.radius = radius
+            self.forward_prediction = partial(self.prediction, self.radius)
+            self.forward_gradient = partial(self.gradient, self.radius)
+            self.forward_jacobian = partial(self.jacobian, self.radius)
+            self.forward_prediction_and_jacobian = partial(self.prediction_and_jacobian, self.radius)
 
     def prediction(self, radius: ndarray, theta: ndarray) -> ndarray:
         """
