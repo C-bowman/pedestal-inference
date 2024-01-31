7c1bf6bf3311df56f5734764ce9efa71cd591763
C-bowman <chris.bowman.physics@gmail.com>
2024-01-31 13:40:42 +0000

    incremented version to 0.3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ copyright = '2022, Chris Bowman'
 author = 'Chris Bowman'
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.3.0"
 # Major.minor version
-version = "0.1.0"
+version = "0.3.0"
 
 # -- General configuration ---------------------------------------------------
 
