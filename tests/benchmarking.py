af7c6f9a9cde767a58643459eca62fc0ab60c2f1
C-bowman <chris.bowman.physics@gmail.com>
2025-03-13 22:15:17 +0000

    Updated the benchmarking configuration

--- tests/benchmarking.py
+++ tests/benchmarking.py
@@ -12,7 +12,7 @@ def profile_func(forward_model, test_params):
 
 
 fit_spatial_channels = arange(107, 130)
-inst_data = build_testing_instrument(n_points=20, spatial_channels=fit_spatial_channels)
+inst_data = build_testing_instrument(n_points=9, spatial_channels=fit_spatial_channels)
 wavelengths, transmissions = build_testing_filters()
 
 
@@ -22,10 +22,9 @@ response = SpectralResponse.calculate_response(
     scattering_angles=test_channel_scattering_angles,
     spatial_channels=fit_spatial_channels,
     ln_te_range = (-1.5, 8.5),
-    n_temps = 128,
-    jit_compile = True,
+    n_temps = 64,
+    jit_compile = "numba",
 )
-
 
 forward_model = SpectrometerModel(
     spectral_response=response,
@@ -34,11 +33,11 @@ forward_model = SpectrometerModel(
 )
 
 
+# build a parameter set for testing
 n_params = 2 * forward_model.model.n_parameters
 test_params = zeros(n_params)
 test_params[forward_model.te_slc] = [1.38, 120., 0.02, 1000., 5.]
 test_params[forward_model.ne_slc] = [1.375, 1e19, 0.015, 5e18, 3e17]
-
 
 # run the function once through to complete jit-compilation
 forward_model.predictions(test_params)
